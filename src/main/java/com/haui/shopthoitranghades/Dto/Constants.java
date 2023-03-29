1c5f1dcbd07e9ea3746df1607c9eecf81d8f669c
VietPham <henny112603@gmail.com>
2023-03-29 23:58:51 +0700

    update

--- src/main/java/com/haui/shopthoitranghades/Dto/Constants.java
+++ src/main/java/com/haui/shopthoitranghades/Dto/Constants.java
@@ -2,5 +2,6 @@ package com.haui.shopthoitranghades.Dto;
 
 public interface Constants {
 	
-	public String UPLOAD_FILE_ROOT = "D:/clone/Website-thoi-trang/uploads/";
+	public String UPLOAD_FILE_ROOT = "D:/clone/Website-thoi-trang/uploads/hahaha";
+	public String UPLOAD_FILE_ROOT1 = "D:/clone/Website-thoi-trang/uploads/";
 }
